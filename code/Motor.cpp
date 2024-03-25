780c95a38313385a077aa21cad0dd1d5819d3ecb
RichieDevDotExe <138896245+RichieDevDotExe@users.noreply.github.com>
2024-03-25 14:57:34 +0000

    Update Motor.cpp
    made speed to PWM signal function

--- code/Motor.cpp
+++ code/Motor.cpp
@@ -2,6 +2,7 @@
 #include <wiringPi.h>
 #include <softPwm.h>
 #include <chrono>
+#include <cmath>
 
 using namespace std;
 using namespace std::chrono;
@@ -16,13 +17,17 @@ int prevState2 = 0;
 int currState2; 
 float stateCount1; 
 float stateCount2; 
-float wheelSize = 70;
+//radius of 2 wheels combined
+float wheelSize = 62.5;
 float wheelEncoderRes = 10;
+float lengthBetweenWheels = 150;
 bool timerCheck = false;
-int moveTime = 2000;
+int moveTime = 1000;
 
-
-
+double deg2rad(double deg)
+{
+    return deg * M_PI / 180;
+}
 
 void stopMotors(){
     cout << "Stopping!" << endl;
@@ -68,68 +73,116 @@ void InitPins(){
     stopMotors();
 }
 
-void setMotorSpeedsPWM(int16_t _motorFL, int16_t _motorFR, int16_t _motorBL, int16_t _motorBR)
+//void setMotorSpeedsPWM(int16_t _motorFL, int16_t _motorFR, int16_t _motorBL, int16_t _motorBR)
+//{
+    //cout << "Set!"<< endl;
+    //// Speed < 0 = backwards Speed > 0 forwards
+    ////get the target direction requested
+    //if (_motorFL < 0){
+        //digitalWrite(16,LOW);
+        //digitalWrite(0,HIGH);
+    //}
+    //else if(_motorFL == 0){
+        //digitalWrite(16,LOW);
+        //digitalWrite(0,LOW);
+    //}
+    //else {
+        //digitalWrite(16,HIGH);
+        //digitalWrite(0,LOW);
+    //}
+
+    //if (_motorFR < 0){
+        //digitalWrite(7,LOW);
+        //digitalWrite(15,HIGH);
+    //}
+    //else if(_motorFR == 0){
+        //digitalWrite(7,LOW);
+        //digitalWrite(15,LOW);
+    //}
+    //else {
+        //digitalWrite(7,HIGH);
+        //digitalWrite(15,LOW);
+    //}
+
+    //if (_motorBL < 0){
+        //digitalWrite(3,LOW);
+        //digitalWrite(4,HIGH);
+    //}
+    //else if(_motorBL == 0){
+        //digitalWrite(3,LOW);
+        //digitalWrite(4,LOW);
+    //}
+    //else {
+        //digitalWrite(3,HIGH);
+        //digitalWrite(4,LOW);
+    //}
+
+    //if (_motorBR < 0){
+        //digitalWrite(1,LOW);
+        //digitalWrite(2,HIGH);
+    //}
+    //else if(_motorBR == 0){
+        //digitalWrite(1,LOW);
+        //digitalWrite(2,LOW);
+    //}
+    //else {
+        //digitalWrite(1,HIGH);
+        //digitalWrite(2,LOW);
+    //}
+
+    ////set speed to invididual pins here.
+    //softPwmWrite(12, abs(_motorFL));
+    //softPwmWrite(13, abs(_motorFR));
+    //softPwmWrite(14, abs(_motorBL));
+    //softPwmWrite(6, abs(_motorBR));
+//}
+
+void setMotorSpeedsPWM(int16_t _motorL, int16_t _motorR)
 {
     cout << "Set!"<< endl;
     // Speed < 0 = backwards Speed > 0 forwards
     //get the target direction requested
-    if (_motorFL < 0){
+    if (_motorL < 0){
         digitalWrite(16,LOW);
         digitalWrite(0,HIGH);
+        digitalWrite(3,LOW);
+        digitalWrite(4,HIGH);
     }
-    else if(_motorFL == 0){
+    else if(_motorL == 0){
         digitalWrite(16,LOW);
         digitalWrite(0,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
     }
     else {
         digitalWrite(16,HIGH);
         digitalWrite(0,LOW);
+        digitalWrite(3,HIGH);
+        digitalWrite(4,LOW);
     }
-
-    if (_motorFR < 0){
+    if (_motorR < 0){
         digitalWrite(7,LOW);
         digitalWrite(15,HIGH);
+        digitalWrite(1,LOW);
+        digitalWrite(2,HIGH);
     }
-    else if(_motorFR == 0){
+    else if(_motorR == 0){
         digitalWrite(7,LOW);
         digitalWrite(15,LOW);
+        digitalWrite(1,LOW);
+        digitalWrite(2,LOW);
     }
     else {
         digitalWrite(7,HIGH);
         digitalWrite(15,LOW);
-    }
-
-    if (_motorBL < 0){
-        digitalWrite(3,LOW);
-        digitalWrite(4,HIGH);
-    }
-    else if(_motorBL == 0){
-        digitalWrite(3,LOW);
-        digitalWrite(4,LOW);
-    }
-    else {
-        digitalWrite(3,HIGH);
-        digitalWrite(4,LOW);
-    }
-
-    if (_motorBR < 0){
-        digitalWrite(1,LOW);
-        digitalWrite(2,HIGH);
-    }
-    else if(_motorBR == 0){
-        digitalWrite(1,LOW);
-        digitalWrite(2,LOW);
-    }
-    else {
         digitalWrite(1,HIGH);
         digitalWrite(2,LOW);
     }
-
-    //set speed to invididual pins here.
-    softPwmWrite(12, abs(_motorFL));
-    softPwmWrite(13, abs(_motorFR));
-    softPwmWrite(14, abs(_motorBL));
-    softPwmWrite(6, abs(_motorBR));
+    
+    softPwmWrite(12, abs(_motorL));
+    softPwmWrite(13, abs(_motorR));
+    softPwmWrite(14, abs(_motorL));
+    softPwmWrite(6, abs(_motorR));
 }
 
 float measureDist(){
@@ -140,7 +193,7 @@ float measureDist(){
     auto timerStart = steady_clock::now(); 
     while(timerCheck != true){
         currState1 = digitalRead(29);
-        currState2 = digitalRead(29);
+        currState2 = digitalRead(28);
         if(currState1 != prevState1){
             prevState1 = currState1;
             stateCount1 += 1;
@@ -152,7 +205,7 @@ float measureDist(){
         
         duration<double, std::milli> timer = (steady_clock::now() - timerStart); 
         //timertemp = duration_cast<milliseconds>(timer).count();
-        cout << timertemp << endl;
+        //cout << timertemp << endl;
         if(duration_cast<milliseconds>(timer).count() == moveTime){
             timerCheck = true;
         }
@@ -167,18 +220,50 @@ float measureDist(){
     cout << distance2 << endl;
     
     
-    cout << "distance1 - " << distance1 << "mm velocity - " << distance1/moveTime << "mm/s" << endl;
-    cout << "distance2 - " << distance2 << "mm velocity - " << distance2/moveTime << "mm/s" << endl;
+    cout << "distance1 - " << distance1 << "mm velocity - " << (distance1/moveTime)*1000 << "mm/s" << endl;
+    cout << "distance2 - " << distance2 << "mm velocity - " << (distance2/moveTime)*1000 << "mm/s" << endl;
     stateCount1 = 0; 
     stateCount2 = 0; 
     timerCheck = false;
     return distance1;
 }
 
+int velocityToPWM(float input){
+    //max velocity input 110mm/s
+    int val;
+    if(input >0){
+        val = round(2.00689 * abs(input) + 14.41695);
+    }
+    else{
+        val = -(round(2.00689 * abs(input) + 14.41695));
+    }
+    cout<<val<<endl;
+    return val;
+}
+
+
+
+float* calVelocity(float inputVelocity,float inputAngle){
+    float* velocity = new float[2];
+    
+    velocity[0] = (2*inputVelocity + deg2rad(inputAngle)*lengthBetweenWheels)/(2*wheelSize);
+    velocity[1] = (2*inputVelocity - deg2rad(inputAngle)*lengthBetweenWheels)/(2*wheelSize);
+    return velocity;
+} 
+
 int main(int argc, char const *argv[]){
     wiringPiSetup();
     InitPins();
-    setMotorSpeedsPWM(125,125,125,125);
+    
+    
+    float* velocity = calVelocity(0,270);
+    cout << velocity[0] << " " << velocity[1]<< endl;
+    
+    int speed1 = velocityToPWM(velocity[0]);
+    int speed2 = velocityToPWM(velocity[1]);
+    
+    
+    setMotorSpeedsPWM(98,-98);
     cout << measureDist() << endl;
     stopMotors();
 }
